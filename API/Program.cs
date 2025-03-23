0e14a613de069d2e196ea05adb1ba9afa3bf4b36
Responsive_Web_design <hemanshupatel32@gmail.com>
2025-03-23 18:36:20 +0530

    Add CORS and Displaying Users to UI and added bootstrap to it

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
- 
-var app = builder.Build();
+
+//Added CORS to make an Request from frontend with diffrent domain to backend has diffrent domain.
+builder.Services.AddCors();
+
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
+//Add to configure the application to user angular website instead dotnet api(localhost:5000/5001)
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
